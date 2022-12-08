1d10569aa4e15a11b1feed49ec18edbb78e13041
TheArchitekt <richardpennymon@gmail.com>
2022-12-08 09:46:24 -0500

    Add Sneaker Class

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ class User(db.Model):
     def __repr__(self):
         return f"<User user_id={self.user_id} user_name={user_name} email={self.user_email}>"
 
+
 class Brand(db.Model):
     """A brand."""
 
@@ -24,3 +25,18 @@ class Brand(db.Model):
 
     def __repr__(self):
         return f"<Brand brand_id={self.brand_id}, brand_name={self.brand_name}>"
+
+
+class Sneaker(db.Model):
+    """A sneaker."""
+
+    __tablename__ = "sneakers"
+
+    sneaker_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    sneaker_name = db.Column(db.String)
+    sneaker_price = db.Column(Numeric(7, 2))
+    sneaker_description = db.Column(db.Text)
+    release_date = db.Column(db.DateTime, db.Index('ix_sneakers_release_date', 'release_date'))
+
+    def __repr__(self):
+        return f"<Sneaker sneaker_id={self.sneaker_id} sneaker_name={self.sneaker_name} release_date={self.release_date}>"
