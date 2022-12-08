0bb18440f3655b44c63db79d0f804e6eeea904ad
TheArchitekt <richardpennymon@gmail.com>
2022-12-08 11:56:57 -0500

    Add dunder name attribute

--- server.py
+++ server.py
@@ -9,3 +9,8 @@ def home():
     """Homepage where users login"""
 
     return render_template("home.html")
+
+
+if __name__ = "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
