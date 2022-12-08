2723f779a6b895553a07d14eda321e63bbc9ac5a
TheArchitekt <richardpennymon@gmail.com>
2022-12-08 14:53:10 -0500

    Add New User function

--- crud.py
+++ crud.py
@@ -1,2 +1,10 @@
 
-from model import db, User, Brand, Sneaker, Favorite
+from model import db, User, Brand, Sneaker, Favorite, connect_to_db, flash
+
+
+def new_user(user_email, user_password):
+    """Creates a new user."""
+
+    user = User(user_email=user_email, user_password=user_password)
+
+    return user
