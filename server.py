0fbf078f96f526946531489634d842626dd13b30
TheArchitekt <richardpennymon@gmail.com>
2022-12-09 09:41:28 -0500

    Add create acocunt function

--- server.py
+++ server.py
@@ -7,9 +7,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    """Homepage where users login"""
+    """Homepage where users login or create an account"""
 
     return render_template("home.html")
+
+
+@app.route("/account", methods=["POST"])
+def create_account():
+    """Creates a user account"""
+
+    user_email = request.form.get("user_email")
+    user_password = request.form.get("user_password")
+
+    user = crud.get_user_email(user_email)
+
+    if user:
+        flash("An account exists with that email.")
+    else:
+        user = crud.new_user(user_email, user_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! You can now log in.")
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
