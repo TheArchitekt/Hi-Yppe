3ba2019eb6940ea2219dfb6e4c99177d648867ed
TheArchitekt <richardpennymon@gmail.com>
2022-12-14 12:27:32 -0500

    Fix New User Function

--- crud.py
+++ crud.py
@@ -2,10 +2,10 @@
 from model import db, User, Brand, Sneaker, Favorite, connect_to_db
 
 
-def new_user(user_email, user_password):
+def new_user(user_email, user_password, user_name):
     """Creates a new user."""
 
-    user = User(user_email=user_email, user_password=user_password)
+    user = User(user_email=user_email, user_password=user_password, user_name=user_name)
 
     return user
 
