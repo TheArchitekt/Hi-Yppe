3f383f18c278a5046d862ff4f0d6584c92ba205d
TheArchitekt <richardpennymon@gmail.com>
2022-12-19 18:50:17 -0500

    Import Strict Undefined

--- server.py
+++ server.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, session, redirect, flash
 from model import connect_to_db, db
 import crud
 
+from jinja2 import StrictUndefined
+
 app = Flask(__name__)
+app.secret_key = "bluezagwa"
+app.jinja_env.undefined = StrictUndefined
 
 
 @app.route("/")
