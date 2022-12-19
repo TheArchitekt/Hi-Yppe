653f7ef41ed1962001daaa922c1566bb79a1dec2
TheArchitekt <richardpennymon@gmail.com>
2022-12-19 18:46:18 -0500

    Modify Seed Database

--- seed_database.py
+++ seed_database.py
@@ -7,3 +7,9 @@ from datetime import datetime
 import crud
 import model
 import server
+
+os.system("dropdb sneakers")
+os.system("createdb sneakers")
+
+model.connect_to_db(server.app)
+model.db.create_all()
