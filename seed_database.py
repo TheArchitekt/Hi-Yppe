acb0fefa79be39e38978b188c9b0cc9eea3e2cd0
TheArchitekt <richardpennymon@gmail.com>
2023-01-02 11:29:39 -0500

    Modify database

--- seed_database.py
+++ seed_database.py
@@ -13,3 +13,25 @@ os.system("createdb sneakers")
 
 model.connect_to_db(server.app)
 model.db.create_all()
+
+with open("data/apidata.json") as f:
+    sneaker_data = json.loads(f.read())
+
+sneakers_in_db = []
+
+for sneaker in sneaker_data:
+    sneaker_name = sneaker["shoeName"]
+    sneaker_brand = sneaker["brand"]
+    sneaker_description = sneaker["description"]
+    sneaker_price = sneaker["retailPrice"]
+    release_date = datetime.strptime(sneaker["releaseDate"], "%Y-%m-%d")
+    sneaker_img_path = sneaker["thumbnail"]
+
+
+
+    db_sneakers = crud.new_sneaker(sneaker_name, sneaker_brand, sneaker_price, sneaker_description, release_date, sneaker_img_path)
+
+    sneakers_in_db.append(db_sneakers)
+
+model.db.session.add_all(sneakers_in_db)
+model.db.session.commit()
