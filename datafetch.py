c96aeed9eb72bdb4ccf9dfe053acf679723742b6
TheArchitekt <richardpennymon@gmail.com>
2023-01-06 09:17:09 -0500

    Add Sneaker Template

--- datafetch.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-
-response = requests.get("https://stockx.com/sneakers")
-
-soup = BeautifulSoup(response.content, 'html.parser')
-
-sneaker_data = []
