cc252a89e11f1adb3cc5ae26a91d4d20ab2fab14
TheArchitekt <richardpennymon@gmail.com>
2023-01-25 13:34:11 -0500

    Remove brand class

--- model.py
+++ model.py
@@ -22,17 +22,6 @@ class User(db.Model):
         return f"<User user_id={self.user_id} user_name={self.user_name} email={self.user_email}>"
 
 
-# class Brand(db.Model):
-#     """A brand."""
-
-#     __tablename__ = "brands"
-
-#     brand_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     brand_name = db.Column(db.String)
-
-#     def __repr__(self):
-#         return f"<Brand brand_id={self.brand_id}, brand_name={self.brand_name}>"
-
 
 class Sneaker(db.Model):
     """A sneaker."""
